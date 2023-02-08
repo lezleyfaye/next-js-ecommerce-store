6e614243535dde587f1cf140962cc94659c9f2c1
Lezley Faye <lezleydavidson@gmail.com>
2023-02-08 15:34:15 +0100

    Added bottle images to public file and beer page

--- app/beer/page.js
+++ app/beer/page.js
@@ -1,11 +1,6 @@
+import Image from 'next/image';
 import Link from 'next/link';
-
-const beers = [
-  { id: 1, name: 'Buntspecht' },
-  { id: 2, name: 'Kohlmeise' },
-  { id: 3, name: 'Mehlschwalbe' },
-  { id: 4, name: 'Rotkehlchen' },
-];
+import { beers } from '../../database/beers';
 
 export default function BeerPage() {
   return (
@@ -17,6 +12,14 @@ export default function BeerPage() {
             <div key={beer.id}>
               <Link href={`/beer/${beers.id}`}>
                 <h3 key={beer.id}>{beer.name}</h3>
+              </Link>
+              <Link href={`/beer/${beers.id}`}>
+                <Image
+                  src={`/bottles/${beer.name}-${beer.id}.jpg`}
+                  alt={beers.name}
+                  width="258"
+                  height="459"
+                />
               </Link>
             </div>
           );
