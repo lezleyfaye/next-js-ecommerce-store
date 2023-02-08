2de455d0d82ad6fd6a433a0a42251c26029fe82d
Lezley Faye <lezleydavidson@gmail.com>
2023-02-08 15:33:08 +0100

    Added database directory

---
+++ database/beers.js
@@ -0,0 +1,8 @@
+import fs from 'node:fs';
+
+export const beers = [
+  { id: 1, name: 'Buntspecht' },
+  { id: 2, name: 'Kohlmeise' },
+  { id: 3, name: 'Mehlschwalbe' },
+  { id: 4, name: 'Rotkehlchen' },
+];
