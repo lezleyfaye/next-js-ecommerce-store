e27e1da4017946726ece5a608b797c9fb96b08a4
Lezley Faye <lezleydavidson@gmail.com>
2023-02-17 08:10:53 +0100

    Add cart page

---
+++ app/cart/page.js
@@ -0,0 +1,32 @@
+// import { cookies } from 'next/headers';
+
+// import { CookieValue } from '../../utils/cookies';
+// import { getParsedCookie, setStringifiedCookie } from '../../utils/cookies';
+
+export const metadata = {
+  title: 'Cart - Wappler Brewing',
+  description: 'Wappler Webshop',
+};
+
+export default function cartPage() {
+  // const beersCookie = cookies().get('beersCookie');
+
+  // let beersCookieParsed = [];
+
+  // if (beersCookie) {
+  //   beersCookieParsed = JSON.parse(beersCookie.value);
+  // }
+  // // console.log(beersCookieParsed);
+
+  // const beersWithQuantity = beers.map((beer) => {
+  //   const beerWithQuantity = { ...beer, quantity: 0 };
+  //   return beerWithQuantity;
+  // });
+
+  return (
+    <div>
+      <h1>cart page</h1>
+      {/* <p>Quantity: {beer.quantity} </p> */}
+    </div>
+  );
+}
